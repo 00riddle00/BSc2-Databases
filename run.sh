df4364de977ca84dfbe0cc40354baa3d4ca6569b
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2020-09-09 23:03:30 +0300

    English folder name

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 export CLASSPATH=$CLASSPATH:lib/postgresql-42.2.5.jar
 
 javac -d . AutoNuoma.java Duombaze.java VartotojoSasaja.java
-java db_auto_nuoma/AutoNuoma
+java db_car_rental/AutoNuoma
 
 
 # Siaip tai norejau sitaip pasileisti, kad butu same kaip intellij, bet kazkodel pyksta kad neranda Main klases (AutoNuoma)
 
-#javac -d out/production/db_auto_nuoma AutoNuoma.java Duombaze.java VartotojoSasaja.java
-#java out/production/db_auto_nuoma/db_auto_nuoma/AutoNuoma
+#javac -d out/production/db_car_rental AutoNuoma.java Duombaze.java VartotojoSasaja.java
+#java out/production/db_car_rental/db_car_rental/AutoNuoma
 
 
